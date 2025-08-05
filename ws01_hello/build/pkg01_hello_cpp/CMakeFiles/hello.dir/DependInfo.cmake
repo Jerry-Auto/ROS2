2411fe2e0c54f61e88760b2b270cbc6c9fa9029b
Jerry-Auto <1763287396@qq.com>
2025-08-05 12:10:51 +0800

    新建了一个ROS项目

---
+++ ws01_hello/build/pkg01_hello_cpp/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/my_code/learn_ros2/ws01_hello/src/pkg01_hello_cpp/src/hello.cpp" "CMakeFiles/hello.dir/src/hello.cpp.o" "gcc" "CMakeFiles/hello.dir/src/hello.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
